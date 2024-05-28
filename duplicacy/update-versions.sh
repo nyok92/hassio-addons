7d0f5c1ee896d785e970024712de934f20963a2e
nyok92 <40342954+nyok92@users.noreply.github.com>
2024-05-29 01:40:47 +0200

    Delete duplicacy directory

--- duplicacy/update-versions.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-version=$(curl -fsSL "https://duplicacy.com/latest_web_version" | jq -re .stable) || exit 1
-json=$(cat VERSION.json)
-jq --sort-keys \
-    --arg version "${version//v/}" \
-    '.version = $version' <<< "${json}" | tee VERSION.json
